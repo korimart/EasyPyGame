09e6165952fb21b80d211809b98799c0b8b889a3
korimart <34030222+korimart@users.noreply.github.com>
2019-11-19 15:10:16 +0900

    camera dist = 1

--- EasyPygame/Components/Camera.py
+++ EasyPygame/Components/Camera.py
@@ -3,7 +3,7 @@ import EasyPygame
 class Camera:
     def __init__(self, initPos=[0, 0]):
         self.pos = initPos
-        self.distance = 2
+        self.distance = 1
 
     def view(self, pos):
         x = pos[0] - self.pos[0] + EasyPygame.getWindowWidth() / 2
