2dd5196e1b3c1b0ec03358abeb3753d1138a0750
korimart <34030222+korimart@users.noreply.github.com>
2019-11-29 19:13:46 +0900

    work1

---
+++ AddOn/WorkOnThis.py
@@ -0,0 +1,30 @@
+# REFERENCES (use the following)
+#
+# DSFactory class methods:
+#
+#   dsFactory.get(type) -> returns a data structure
+#       type is either "queue", "stack", "graph"
+#  
+# Queue class methods:
+# 
+#   queue.push(item)
+#   queue.pop()
+#
+# Map class methods:
+#
+#   map.getTerrain(x, y) -> returns 0 (nothing) or 1 (hazard)
+#       out-of-scope values also return 1
+
+class Terrain:
+    NOTHING = 0
+    HAZARD = 1
+
+class BFS:
+    def __init__(self, dsFactory):
+        self.dsFactory = dsFactory
+
+    def findPath(self, pointA, pointB, map):
+        # pointA and pointB are tuples of length 2
+        # return a list of points on the calculated path
+        queue = self.dsFactory("queue")
+        pass
