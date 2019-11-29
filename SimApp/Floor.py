2a16c73d40180c12fa55ec31f9457928a97f12d3
Korimart <korimart@office.uos.ac.kr>
2019-11-30 01:05:39 +0900

    making floor

--- SimApp/Floor.py
+++ SimApp/Floor.py
@@ -1,9 +1,41 @@
 from EasyPygame.Components import *
 
+class Terrain:
+    NOTHING = 0
+    HAZARD = 1
+    BLOB = 2
+
 class Floor(GameObject):
     def __init__(self, scene, width, height):
         super().__init__(scene, "Floor")
-        self.covered = []
+        self.terrain = []
         self.uncovered = []
         self.width = width
         self.height = height
+        self.mazeGenerator = None
+
+        self.wall = GameObject(scene, "Wall")
+        # self.wall.addTextureView(InstancedTextureView())
+
+    def randomize(self):
+        self.terrain = self.mazeGenerator.generate(self.width, self.height)
+        self.uncovered = [[0 for _ in range(width)] for _ in range(height)]
+
+    def restart(self, map):
+        pass
+
+    def senseBlob(self, x, y):
+        ret = self.terrain[x][y]
+        if ret == Terrain.BLOB:
+            self.uncovered[x][y] = Terrain.BLOB
+            return True
+
+        return False
+
+    def senseHazard(self, x, y):
+        ret = self.terrain[x][y]
+        if ret == Terrain.HAZARD:
+            self.uncovered[x][y] = Terrain.HAZARD
+            return True
+
+        return False
