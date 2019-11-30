66222aa23465355aa6858466d471917377b35747
Korimart <korimart@office.uos.ac.kr>
2019-11-30 17:38:43 +0900

    AddOn assemble

---
+++ AddOn/Algorithm.py
@@ -0,0 +1,125 @@
+def sanitiyCheck(locations, mmap):
+        lst = []
+        for p in locations:
+            if mmap.getTerrain(p[0], p[1]) == 0:
+                lst.append(p)
+        return lst
+
+def calculateCoordinates(coord, direction):
+    newCoord = list(coord)
+    if direction == 0:
+        newCoord[1] += 1
+    elif direction == 1:
+        newCoord[0] += 1
+    elif direction == 2:
+        newCoord[1] -= 1
+    elif direction == 3:
+        newCoord[0] -= 1
+    return tuple(newCoord)
+
+def possiblePositions(curPos, mmap):
+    positions = list()
+    for direc in range(4):
+        positions.append(calculateCoordinates(curPos, direc))
+    return sanitiyCheck(positions, mmap)
+
+def ManhattanDistance2D(x1, x2):
+        d = 0
+        d += abs(x1[0] - x2[0])
+        d += abs(x1[1] - x2[1])
+        return d
+
+class Terrain:
+    NOTHING = 0
+    HAZARD = 1
+
+class VisitOrderProducer:
+    def __init__(self, algorithm):
+        self.algorithm = algorithm
+
+    def findPath(self, startingPoint, targetList, mmap):
+        # implemenet this
+        # startingPoint is a tuple
+        # targetList is a list of tuples that need to be visited
+        # mmap is a Map class instance
+        # return an optimized path that starts from startingPoint and visits all tuples in targetList
+        path = []
+        while(len(targetList) != 0):
+            paths = []
+            for searchPoint in targetList:
+                paths.append(self.algorithm.findPath(startingPoint, searchPoint, mmap))
+                if paths[-1] == None:
+                    return None
+            subPath = min(paths, key=len)
+            del subPath[0]
+            if len(subPath) > 0:
+                PointA = subPath[-1]
+            targetList.remove(PointA)
+            path += subPath
+        return path
+
+class BFS:
+    def __init__(self, dsFactory):
+        self.dsFactory = dsFactory
+
+    def findPath(self, pointA, pointB, mmap):
+        # implement this
+        # pointA and pointB are tuples of length 2
+        # return a list of points on the calculated path
+        queue = self.dsFactory.getQueue()
+
+        visited = set()
+        queue.push([pointA])
+        visited.add(pointA)
+        while(len(queue) != 0):
+            # paint()
+            path = queue.pop()
+            current = path[-1]
+            if pointB == current:
+                return path
+            possiblePos = possiblePositions(current, mmap)
+            for position in possiblePos:
+                if position not in visited:
+                    visited.add(position)
+                    copiedPath = path.copy()
+                    copiedPath.append(position)
+                    queue.push(copiedPath)
+        return None
+
+class IDAstar:
+    def __init__(self, dsFactory):
+        self.dsFactory = dsFactory
+
+    def findPath(self, pointA, pointB, mmap):
+        stack = self.dsFactory.getStack()
+        bound = ManhattanDistance2D(pointA, pointB)
+        stack.push(pointA)
+        while True:
+            t = self._IDA_star_search(pointB, stack, 0, bound, mmap)
+            if t == "Found":
+                return stack.getList()
+            if t == None:
+                return None
+            bound = t
+
+    def _IDA_star_search(self, end, path, g, bound, mmap):
+        node = path.peek()
+        f = g + ManhattanDistance2D(node, end)
+        if f > bound:
+            return f
+        if end == node:
+            return "Found"
+        minimum = None
+        for neighbour in possiblePositions(node, mmap):
+            if neighbour not in path.getList():
+                path.push(neighbour)
+                t = self._IDA_star_search(end, path, g + 1, bound, mmap)
+                if t == "Found":
+                    return "Found"
+                if t != None:
+                    if minimum == None:
+                        minimum = t
+                    elif t < minimum:
+                        minimum = t
+                path.pop()
+        return minimum
---
+++ AddOn/Behavior.py
@@ -0,0 +1,101 @@
+from AddOn.DSFactory import *
+from AddOn.Algorithm import *
+
+class BehaviorGoFast:
+    def __init__(self):
+        self.dsFactory = DSFactory()
+        self.algorithm = BFS(self.dsFactory)
+        self.visitOrderProducer = VisitOrderProducer(self.algorithm)
+        #
+        self.pathNeedsUpdate = True
+        self.position = None
+        self.direction = None
+        self.coordinates = None
+
+    def go(self, robot, mmap, pathFinder):
+        # implement this
+        # DO NOT check time and memory
+        while(len(mmap.getUnvisitedSearchPoints()) > 0):
+            self._takeAStep(robot, mmap)
+
+    def _takeAStep(self, robot, mmap):
+        self._prepToMove(robot, mmap) #Base
+        self._updatemMap(robot, mmap) #Sub
+        self._findPath(robot, mmap) #Sub
+        self._move(robot, mmap) #Base
+
+    def _posToCoord(self, position):
+        return (position[0], position[1])
+
+    def _prepToMove(self, robot, mmap):
+        self.position = robot.getPos()
+        self.direction = self._posToDirection(self.position)
+        self.coordinates = self._posToCoord(self.position)
+        mmap.pathTaken.append(self.coordinates)
+        if self._posToCoord(mmap.currentPos()) != self.coordinates:
+            self.pathNeedsUpdate = True
+
+    def _move(self, robot, mmap):
+        if len(mmap.pathToBeTaken) > 0:
+                self._moveInDirection(robot, self.coordinates, self.direction, mmap.nextDestination())
+
+    def _updatemMap(self, robot, mmap):
+        mmap.update(
+                self.coordinates,
+                self._getHazardData(robot, mmap, self.position),
+                self._getBlobData(robot, self.position))
+
+    # Yet to be changed in SIM or Robot
+    def _getBlobData(self, robot, position):
+        blobs = []
+        direction = 0
+        coordinates = [position[0], position[1]]
+        rawData = robot.senseBlob()
+
+        for raw in rawData:
+            if raw:
+                blobs.append(self._calculateCoordinates(coordinates, direction))
+            direction = (direction + 1) % 4
+        return blobs
+
+    def _findPath(self, robot, mmap):
+        if mmap.isOnPath or self.pathNeedsUpdate:
+                mmap.pathToBeTaken = self.visitOrderProducer.findPath(
+                    self.coordinates, mmap.getUnvisitedSearchPoints(), mmap)
+        if mmap.pathToBeTaken == None:
+            raise RuntimeError("mmap.pathToBeTaken == None")
+
+    def _getHazardData(self, robot, mmap, position):
+        hazards = []
+        direction = position[2]
+        coordinates = [position[0], position[1]]
+
+        if robot.senseHazard():
+                frontCoord = self._calculateCoordinates(coordinates, direction)
+                hazards.append(frontCoord)
+        return hazards
+
+    def _posToDirection(self, position):
+        return position[2]
+
+    def _calculateCoordinates(self, coord, direction):
+        newCoord = list(coord)
+        if direction == 0:
+            newCoord[1] += 1
+        elif direction == 1:
+           newCoord[0] += 1
+        elif direction == 2:
+            newCoord[1] -= 1
+        elif direction == 3:
+            newCoord[0] -= 1
+        return tuple(newCoord)
+
+    def _moveInDirection(self, robot, curLocation, direction, destination):
+        #nextLocation = self.path.pop(0)
+        while(destination != self._calculateCoordinates(curLocation, direction)):
+            robot.rotate()
+            direction = self._nextDirection(direction)
+        robot.move()
+
+    def _nextDirection(self, direction):
+        return (direction + 1) % 4
