ed10c7a5cf097fe23f69953012d79d99f55f3566
Plefy <55909768+Plefy@users.noreply.github.com>
2019-12-01 15:31:49 +0900

    GoSlow pathNeedsUpdate

--- AddOn/Behavior.py
+++ AddOn/Behavior.py
@@ -49,10 +49,45 @@ class BehaviorGoFast:
             self._takeAStep(robot, mmap, pathFinder)
 
     def _takeAStep(self, robot, mmap, pathFinder):
-        self._prepToMove(robot, mmap) #Base
-        self._updatemMap(robot, mmap) #Sub
-        self._findPath(robot, mmap, pathFinder) #Sub
-        self._move(robot, mmap) #Base
+        self.position = robot.getPos()
+        self.direction = _posToDirection(self.position)
+        self.coordinates = _posToCoord(self.position)
+        mmap.pathTaken.append(self.coordinates)
+        
+        if _posToCoord(mmap.currentPos()) != self.coordinates:
+            self.pathNeedsUpdate = True
+
+        if self.pathNeedsUpdate:
+            mmap.pathToBeTaken = pathFinder.findPath(
+                    self.coordinates, mmap.getUnvisitedSearchPoints(), mmap)
+            if mmap.pathToBeTaken == None:
+                raise RuntimeError("mmap.pathToBeTaken == None")
+        
+        self._faceThisPoint(robot, mmap.peekNextDestination())
+        mmap.update(
+                self.coordinates,
+                self._getHazardData(robot, mmap),
+                _getBlobData(robot, self.position))
+
+        while(mmap.isOnNextStep()):
+            mmap.pathToBeTaken = pathFinder.findPath(
+                    self.coordinates, mmap.getUnvisitedSearchPoints(), mmap)
+            if mmap.pathToBeTaken == None:
+                raise RuntimeError("mmap.pathToBeTaken == None")
+            self._faceThisPoint(robot, mmap.peekNextDestination())
+            self._updateHazard(mmap, robot)
+            
+        mmap.popNextDestination()
+        robot.move()
+
+    def _faceThisPoint(self, robot, destination):
+        while(destination != _calculateCoordinates(
+            self.coordinates, self.direction)):
+            robot.rotate()
+            self.direction = _nextDirection(self.direction)
+
+    def _updateHazard(self, mmap, robot):
+        mmap.update(self.coordinates, self._getHazardData(robot, mmap), [])
 
     def _prepToMove(self, robot, mmap):
         self.position = robot.getPos()
@@ -71,9 +106,83 @@ class BehaviorGoFast:
 
     def _move(self, robot, mmap):
         if len(mmap.pathToBeTaken) > 0:
-                self._moveInDirection(robot, self.coordinates, self.direction, mmap.nextDestination())
+                self._moveInDirection(
+                    robot, self.coordinates, self.direction,
+                    mmap.popNextDestination())
 
-    def _updatemMap(self, robot, mmap):
+    def _updateMap(self, robot, mmap):
+        mmap.update(
+                self.coordinates,
+                self._getHazardData(robot, mmap),
+                _getBlobData(robot, self.position))
+
+    def _getHazardData(self, robot, mmap): 
+        hazards = []
+        if robot.senseHazard():
+                frontCoord = _calculateCoordinates(
+                    self.coordinates, self.direction)
+                hazards.append(frontCoord)
+        return hazards
+
+        """
+        hazards = []
+        direction = position[2]
+        coordinates = [position[0], position[1]]
+
+        if robot.senseHazard():
+                frontCoord = self._calculateCoordinates(coordinates, direction)
+                hazards.append(frontCoord)
+        return hazards
+        """
+
+    def _moveInDirection(self, robot, curLocation, direction, destination):
+        #nextLocation = self.path.pop(0)
+        while(destination != _calculateCoordinates(curLocation, direction)):
+            robot.rotate()
+            direction = _nextDirection(direction)
+        robot.move()
+
+class GoSlow:
+    def __init__(self):
+        self.pathNeedsUpdate = True
+        self.position = None
+        self.direction = None
+        self.coordinates = None
+
+    def go(self, robot, mmap, pathFinder):
+        # implement this
+        # DO NOT check time and memory
+        while(len(mmap.getUnvisitedSearchPoints()) > 0):
+            self._takeAStep(robot, mmap, pathFinder)
+
+    def _takeAStep(self, robot, mmap, pathFinder):
+        self._prepToMove(robot, mmap) #Base
+        self._updateMap(robot, mmap) #Sub
+        self._findPath(robot, mmap, pathFinder) #Sub
+        self._move(robot, mmap) #Base
+
+    def _prepToMove(self, robot, mmap):
+        self.position = robot.getPos()
+        self.direction = _posToDirection(self.position)
+        self.coordinates = _posToCoord(self.position)
+        mmap.pathTaken.append(self.coordinates)
+        if _posToCoord(mmap.currentPos()) != self.coordinates:
+            self.pathNeedsUpdate = True
+
+    def _findPath(self, robot, mmap, pathFinder):
+        if mmap.isOnPath or self.pathNeedsUpdate:
+                mmap.pathToBeTaken = pathFinder.findPath(
+                    self.coordinates, mmap.getUnvisitedSearchPoints(), mmap)
+        self.pathNeedsUpdate = False
+        if mmap.pathToBeTaken == None:
+            raise RuntimeError("mmap.pathToBeTaken == None")
+
+    def _move(self, robot, mmap):
+        if len(mmap.pathToBeTaken) > 0:
+                self._moveInDirection(robot, self.coordinates, self.direction,
+                    mmap.popNextDestination())
+
+    def _updateMap(self, robot, mmap):
         mmap.update(
                 self.coordinates,
                 self._getHazardData(robot, mmap, self.position),
@@ -112,4 +221,3 @@ class BehaviorGoFast:
             robot.rotate()
             direction = _nextDirection(direction)
         robot.move()
-
